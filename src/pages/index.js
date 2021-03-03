fd9af185adfa5f359ccd3a12643003ecc01f4b21
Elisha Mallard <elishashestov@gmail.com>
2021-03-03 08:24:32 +0200

    everything new

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import Layout from "../components/layout"
 export default () => (
 
    <Layout>
+      <h2 style={{ color:"red", fontSize:"42px"}}>
       Finally
+      </h2>
    </Layout>
     
       
